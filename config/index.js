fac5b33aa3c59109a6d53903ddc8b8d2e30e79cb
sedhal <sedhal@globalvoxinc.com>
2025-02-14 15:02:06 +0530

    update env file

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 module.exports = {
   URL: process.env.NODE_ENVIRONMENT === 'local' ? `mongodb://localhost:27017/iotfleetdb` : 
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`,
-  // `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:27017/${process.env.DB_NAME}`,
+  // `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`,
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:27017/${process.env.DB_NAME}`,
   SECRET: process.env.SECRET,
   TOKEN_EXPIRATION: process.env.TOKEN_EXPIRATION,
   REQUEST_TIMEOUT: process.env.REQUEST_TIMEOUT,
